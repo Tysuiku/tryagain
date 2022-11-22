af12ecd310e810fb40dd8c43dc42c1ea7f474e98
Tysuiku <Timothydong41@gmail.com>
2022-11-22 12:32:29 -0500

    trying to code again with bad mental

--- W1D1/advanced_methods_exercise/01_coprime.rb
+++ W1D1/advanced_methods_exercise/01_coprime.rb
@@ -3,6 +3,12 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 
+def coprime?(num_1, num_2)
+  (2...num_1).each do |i|
+    return false if num_1 % i == 0 || num_2 % i == 0
+  end
+  true
+end
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
