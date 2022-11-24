aacc5a78021c5686deba98f511358be59121b80f
Tysuiku <Timothydong41@gmail.com>
2022-11-24 04:21:47 -0500

    cant sleep

--- W1D2/blocks_project/lib/part_1.rb
+++ W1D2/blocks_project/lib/part_1.rb
@@ -3,5 +3,9 @@ def select_even_nums(nums)
 end
 
 def reject_puppies(dogs)
-  dogs.reject { |dog| dog[:age] < 2 }
+  dogs.reject { |dog| dog["age"] <= 2 }
+end
+
+def count_positive_subarrays(arr)
+  arr.count { |sub_arr| sub_arr.sum >= 1 }
 end
