6f6e56a478ecade223334947e05f421fe62c6748
Tysuiku <Timothydong41@gmail.com>
2022-11-23 19:19:50 -0500

    should come back to compressed

--- W1D1/algorithmic_strategies_exercise/05_compress_str.rb
+++ W1D1/algorithmic_strategies_exercise/05_compress_str.rb
@@ -2,19 +2,24 @@
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
 def compress_str(str)
-  hash = {}
-  str = ""
-  str.each_char do |char|
-    if !hash[char]
-      hash[char] = 1
+  result = ""
+  i = 0
+  while i < str.length
+    char = str[i]
+    count = 0
+    while char == str[i]
+      count += 1
+      i += 1
+    end
+
+    if count > 1
+      result += count.to_s + char
     else
-      hash[char] += 1
+      result += char
     end
   end
-  hash.each do |k, v|
-    str += k + v
-  end
-  str
+
+  result
 end
 
 p compress_str("aaabbc")        # => "3a2bc"
