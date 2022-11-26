6789ea17a9a87a078802c8e31dd01974c2ea462a
Tysuiku <Timothydong41@gmail.com>
2022-11-25 21:28:45 -0500

    i dont want to code today

--- W1D3/rspec_exercise_2/lib/part_1.rb
+++ W1D3/rspec_exercise_2/lib/part_1.rb
@@ -15,4 +15,22 @@ def partition(arr, n)
 end
 
 def merge(hash1, hash2)
+  hash = {}
+  hash1.each { |k, v| hash[k] = v }
+  hash2.each { |k, v| hash[k] = v }
+  hash
+end
+
+def censor(sentence, curse_words)
+  new_sen = []
+  vowels = "aeiouAEIOU"
+  sentence.split(" ").each do |word|
+    if curse_words.include?(word)
+      word.each_char.with_index { |char| word[i] = "*" if vowels.include?(char) }
+      new_sen << word
+    else
+      new_sen << word
+    end
+  end
+  new_sen
 end
