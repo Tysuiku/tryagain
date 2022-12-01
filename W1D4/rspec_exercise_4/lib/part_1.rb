21fe12969ce078870f402670c4f349cea5f4dbc3
Tysuiku <Timothydong41@gmail.com>
2022-11-30 21:51:58 -0500

    was a art day hench the lack of code

--- W1D4/rspec_exercise_4/lib/part_1.rb
+++ W1D4/rspec_exercise_4/lib/part_1.rb
@@ -10,3 +10,7 @@ def hash_select(hash, &block)
   hash.reject! { |k, v| k[v] if !block.call(k, v) }
   hash
 end
+
+def xor_select(arr, prc1, prc2)
+  arr.select { |ele| ele if prc1.call(ele) || prc2.call(ele) && !(prc1.call(ele) && prc2.call(ele)) }
+end
