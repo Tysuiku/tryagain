cc74004a86bbde9329f1e204fe64590d1176fae7
Tysuiku <Timothydong41@gmail.com>
2022-12-08 18:13:53 -0500

    continue later

--- W1D1/algorithmic_strategies_exercise/04_peak_finder.rb
+++ W1D1/algorithmic_strategies_exercise/04_peak_finder.rb
@@ -3,6 +3,19 @@
 # An element is considered a "peak" if it is greater than both it's left and right neighbor.
 # The first or last element of the array is considered a "peak" if it is greater than it's one neighbor.
 
+def peak_finder(arr)
+  peak = []
+
+  (1...arr.length - 1).each do |i|
+    if arr[i] > arr[i - 1] && arr[i] > arr[i + 1]
+      peak << arr[i]
+    end
+  end
+
+  peak.unshift(arr[0]) if arr[0] > arr[1]
+  peak << arr[-1] if arr[-1] > arr[-2]
+  peak
+end
 
 p peak_finder([1, 3, 5, 4])         # => [5]
 p peak_finder([4, 2, 3, 6, 10])     # => [4, 10]
--- W1D1/algorithmic_strategies_exercise/05_compress_str.rb
+++ W1D1/algorithmic_strategies_exercise/05_compress_str.rb
@@ -2,6 +2,23 @@
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
 
+def compress_str(str)
+  count = 1
+
+  compress = ""
+  str.each_char.with_index do |char, i|
+    if str[i] == str[i + 1]
+      count += 1
+    elsif count == 1
+      compress += char
+    else
+      compress += count.to_s + char
+      count = 1
+    end
+  end
+
+  compress
+end
 
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
