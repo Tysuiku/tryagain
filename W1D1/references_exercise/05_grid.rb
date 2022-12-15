1b1f3dcb27e5590b4c5a251c4e7b6b70c5226440
Tysuiku <Timothydong41@gmail.com>
2022-12-14 22:48:38 -0500

    testing wsl setup back at home

--- W1D1/references_exercise/05_grid.rb
+++ W1D1/references_exercise/05_grid.rb
@@ -1,6 +1,6 @@
 # Write a method, grid(n, m), that accepts two numbers as args.
 # The method should return a 2D array with n rows and m columns (this means that there are n subarrays, each of length m).
-
+#testing
 def grid(n, m)
   Array.new(n) { Array.new(m) }
 end
