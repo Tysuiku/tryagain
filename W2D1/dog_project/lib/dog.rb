600895c0683c19282f3ab86bc0486196a0827db1
Tysuiku <Timothydong41@gmail.com>
2022-12-22 15:48:25 -0500

    finished dog project

--- W2D1/dog_project/lib/dog.rb
+++ W2D1/dog_project/lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
+  attr_accessor :name, :breed, :age
 
+  def initialize(name, breed, age, bark, favorite_foods)
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+  end
+
+  def bark
+    return @bark.upcase if @age > 3
+    @bark.downcase
+  end
+
+  def favorite_foods
+    @favorite_foods
+  end
+
+  def favorite_food?(food)
+    favorite_foods.map(&:downcase).include? food.downcase
+  end
 end
