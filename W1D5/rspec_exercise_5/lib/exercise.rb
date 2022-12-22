f9009ff9486bf315ece82759a8a1cff502be0035
Tysuiku <Timothydong41@gmail.com>
2022-12-22 11:13:44 -0500

    finished rspec 5 at work

--- W1D5/rspec_exercise_5/lib/exercise.rb
+++ W1D5/rspec_exercise_5/lib/exercise.rb
@@ -34,7 +34,55 @@ def maximum(arr, &block)
 end
 
 def my_group_by(arr, &block)
-  hash = Hash.new(0)
-  arr.each { |i| hash[block.call(i)] << i }
+  hash = {}
+  arr.each do |i|
+    if hash[block.call(i)]
+      hash[block.call(i)] << i
+    else
+      hash[block.call(i)] = [i]
+    end
+  end
+
   hash
+end
+
+def max_tie_breaker(arr, prc, &block)
+  return nil if arr.length == 0
+  max = arr[0]
+  arr.each do |i|
+    result_el = block.call(i)
+    result_max = block.call(max)
+    if result_el > result_max
+      max = i
+    elsif result_el == result_max && prc.call(i) > prc.call(max)
+      max = i
+    end
+  end
+
+  max
+end
+
+def change_word(word)
+  indicies = vowel_indicies(word)
+  word[indicies.first..indicies.last]
+end
+
+def vowel_indicies(word)
+  vowels = "aeiou"
+  indicies = []
+  word.each_char.with_index { |char, i| indicies << i if vowels.include?(char) }
+  indicies
+end
+
+def silly_syllables(sen)
+  words = sen.split(" ")
+  arr = words.map do |word|
+    if vowel_indicies(word).length < 2
+      word
+    else
+      change_word(word)
+    end
+  end
+
+  arr.join(" ")
 end
