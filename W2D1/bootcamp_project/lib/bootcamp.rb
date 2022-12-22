a4fb973aac33b0510788a12cdf5bf2699b1f692c
Tysuiku <Timothydong41@gmail.com>
2022-12-22 14:00:28 -0500

    working on bootcamp proj

--- W2D1/bootcamp_project/lib/bootcamp.rb
+++ W2D1/bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,31 @@
 class Bootcamp
-  
+  attr_reader :name, :slogan, :teachers, :students
+
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new { |h, k| h[k] = [] }
+  end
+
+  def hire(teacher)
+    @teachers << teacher
+  end
+
+  def enroll(student)
+    if @students.length < @student_capacity
+      @students << student
+      return true
+    end
+    false
+  end
+
+  def enrolled?(student)
+    @students.include? student
+  end
+
+  def student_to_teacher_ratio
+  end
 end
