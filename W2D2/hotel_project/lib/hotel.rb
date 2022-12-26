670628fe12b333c854ba49be63eef7212c3c6cd2
Tysuiku <Timothydong41@gmail.com>
2022-12-26 12:52:56 -0500

    working on hotel project

--- W2D2/hotel_project/lib/hotel.rb
+++ W2D2/hotel_project/lib/hotel.rb
@@ -1,5 +1,22 @@
 require_relative "room"
 
 class Hotel
-  
+  attr_reader :rooms
+
+  def initialize(name, hash)
+    @name = name
+    @rooms = {}
+    hash.each do |room_name, capacity|
+      @rooms[room_name] = Room.new(capacity)
+    end
+  end
+
+  def name
+    @name.split(" ").map(&:capitalize).join(" ")
+  end
+
+  def room_exists?(room_name)
+    return true if @rooms[room_name]
+    false
+  end
 end
--- W2D2/hotel_project/lib/room.rb
+++ W2D2/hotel_project/lib/room.rb
@@ -1,3 +1,25 @@
 class Room
-  
+  attr_reader :capacity, :occupants
+
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
+
+  def full?
+    @occupants.length >= @capacity
+  end
+
+  def available_space
+    @capacity - @occupants.length
+  end
+
+  def add_occupant(name)
+    if full?
+      return false
+    else
+      @occupants << name
+    end
+    true
+  end
 end
