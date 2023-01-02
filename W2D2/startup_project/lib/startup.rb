5c67a84c204fd03885d7c269e17a431e573fef77
Tysuiku <Timothydong41@gmail.com>
2023-01-02 09:32:34 -0500

    started start up project

--- W2D2/startup_project/lib/startup.rb
+++ W2D2/startup_project/lib/startup.rb
@@ -1,5 +1,23 @@
 require "employee"
 
 class Startup
+  attr_reader :name, :funding, :salaries, :employees
 
+  def initialize(name, funding, salaries)
+    @name = name
+    @funding = funding
+    @salaries = salaries
+    @employees = []
+  end
+
+  def valid_title?(title)
+    @salaries.include? title
+  end
+
+  def >(other_start_up)
+    @funding > other_start_up.funding
+  end
+
+  def hire(employee_name, title)
+  end
 end
